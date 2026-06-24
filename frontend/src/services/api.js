aa0c0285fee85a9f418733ea7941465bf0975223
Dikshith <ddikshith61@gmail.com>
2026-06-24 08:49:22 +0530

    first commit

---
+++ frontend/src/services/api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API = axios.create({
+    baseURL: "http://localhost:5000"
+});
+
+export const getLeetCodeProfile = async (username) => {
+    const response = await API.get(
+        `/api/profile/leetcode/${username}`
+    );
+
+    return response.data;
+};
+export const getCodeforcesProfile = async (
+    username
+) => {
+
+    const response = await API.get(
+        `/api/profile/codeforces/${username}`
+    );
+
+    return response.data;
+};
+export const getCodeforcesHistory =
+async (username) => {
+
+    const response = await API.get(
+        `/api/profile/codeforces/${username}/history`
+    );
+
+    return response.data;
+};
